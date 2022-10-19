a0d1fd5784aac569cd6cc2c6b1147e1ba78d9444
nimplay <nimrod7day@gmail.com>
2022-10-19 13:17:18 -0400

    show post less than 3 last updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
   root 'users#index'
-  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/new', to: 'users#new', as: :new_user
+  get 'users/:id', to: 'users#show', as: :user
 end
