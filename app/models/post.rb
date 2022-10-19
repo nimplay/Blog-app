5f0d057ead4b25aa13f07d5ea6f92e34a736cf0c
nimplay <nimrod7day@gmail.com>
2022-10-19 17:42:28 -0400

    processing data in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  belongs_to :user, class_name: 'user'
+  has_many :comments
+  has_many :likes
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 80 }
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_post_counter
+
+  def five_most_recent_comments
+    Comment.where(postId: self).order(created_at: :desc).limit(5)
+  end
+
+  private
+  def update_post_counter
+    author.increment!(:postCounter)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'users#index'
   get 'users/new', to: 'users#new', as: :new_user
   get 'users/:id', to: 'users#show', as: :user
+  get 'users/:id/posts', to: 'posts#index', as: :user_posts
 end
