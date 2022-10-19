df4bec653af3d539ce50ab4fb63a0c9fd826390d
nimplay <nimrod7day@gmail.com>
2022-10-19 17:47:13 -0400

    fixing linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,6 @@ class PostsController < ApplicationController
   end
 
   def show
-
     @user = User.find(params[:id])
     @post = Post.find(params[:id])
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -14,6 +14,7 @@ class Post < ApplicationRecord
   end
 
   private
+
   def update_post_counter
     author.increment!(:postCounter)
   end
