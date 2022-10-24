ee70db7979803f449e33521b9570e7b383a8e662
nimplay <nimrod7day@gmail.com>
2022-10-24 14:00:06 -0400

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'users/new', to: 'users#new', as: :new_user
   get 'users/:id', to: 'users#show', as: :user
   get 'users/:id/posts', to: 'posts#index', as: :user_posts
+  get 'users/:id/posts/:post_id', to: 'posts#show', as: :post
 end
