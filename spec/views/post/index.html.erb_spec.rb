be3a5a1dc0ae26c473328b02f1908e9e7b13efb1
gedewon <gedewon.hayle.9@gmail.com>
2022-11-01 00:35:55 +0300

    Add Post Page user can see  profile picture

---
+++ spec/views/post/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'User post index page', type: :feature do
+
+  before(:all) do 
+        @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+            bio: 'Teacher from Mexico.', postCounter: 0)
+
+        @first_post = Post.create(authorId: @first_user.id, title: 'Hello', text: 'This is my first post',
+            commentsCounter: 0, likesCounter: 0)
+        @second_post = Post.create(authorId: @first_user.id, title: 'Hello', text: 'This is my second post',
+            commentsCounter: 0, likesCounter: 0)
+        @third_post = Post.create(authorId: @first_user.id, title: 'Hello', text: 'This is my third post',
+            commentsCounter: 0, likesCounter: 0)
+        @fourth_post = Post.create(authorId: @first_user.id, title: 'Hello', text: 'This is my fourth post',
+            commentsCounter: 0, likesCounter: 0)
+  end 
+
+  it 'I can see the users profile picture.' do
+    visit "/users/#{@first_user.id}/posts"
+    expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']")
+  end
+
+end
