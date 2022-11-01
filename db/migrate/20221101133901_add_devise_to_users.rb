20196d8ca5aa6f5706bc9a5e9f7ed460dce2860d
nimplay <nimrod7day@gmail.com>
2022-11-01 10:02:08 -0400

    add devise

--- db/migrate/20221101133901_add_devise_to_users.rb
+++ db/migrate/20221101133901_add_devise_to_users.rb
@@ -1,4 +1,4 @@
-class AddPasswordToUsers < ActiveRecord::Migration[7.0]
+class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
