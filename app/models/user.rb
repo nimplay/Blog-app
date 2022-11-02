e08219f513d0305ee9cfa23e3717d92300b7cbde
nimplay <nimrod7day@gmail.com>
2022-11-02 15:07:11 -0400

    deleting extra migrations and fixing the rollback error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'authorId'
   has_many :likes, foreign_key: 'authorId'
 
-  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :postCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  # validates :postCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_posts
     posts.reverse_order.first(3)
