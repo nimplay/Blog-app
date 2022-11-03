a36d4da34c79fe7ac2b64989fad2fe26548a069a
nimplay <nimrod7day@gmail.com>
2022-11-03 10:37:18 -0400

    log out user and home buttom

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.admin?
-      can :manage, :all
-    else
-      can :manage, Post, author: user
-      can :create, Comment
-      can :manage, Comment, author: user
-      can :create, Like
-    end
+   can :read, Post, public: true
+    can :read, Comment, public: true
+
+    return unless current_user.present?
+
+    can :read, Post, author: current_user
+    can :destroy, Comment, author_id: current_user
   end
 end
